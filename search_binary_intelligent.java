cd182b0569f2b9264435dc06419a9d7a63e5a85a
Telukunta Vijay Abhinav <62216101+Abhinav-Telukunta@users.noreply.github.com>
2020-09-02 11:35:36 +0530

    Search element in a rotated array
    Use Binary Search to find element in rotated array

---
+++ search_binary_intelligent.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class search{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;++i) arr[i]=sc.nextInt();
+        int key=sc.nextInt();
+        int low=0,high=n-1,pivot=0;
+        while(low<=high){
+            pivot=(low+high)/2;
+            if(pivot+1<n && arr[pivot]>arr[pivot+1]){
+                break;
+            }
+            else if(arr[pivot]<arr[0]) high=pivot-1;
+            else low=pivot+1;
+        }
+        int ridx=(pivot+1)%n;
+        //System.out.println(ridx);
+        int mid=0;
+        int right=(ridx-1);
+        if(right<0) right=n-1;
+        if(key<arr[ridx] || (key>arr[right])){
+            System.out.println(-1);
+            System.exit(0);
+        }
+        low=0;
+        high=n-1;
+        int flag=0;
+        if(key>arr[high]) high=ridx-1;
+        else if(key<arr[high]) low=ridx;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(key==arr[mid]){
+                System.out.println(mid);
+                flag=1;
+                break;
+            }
+            else if(key>arr[mid]) low=mid+1;
+            else if(key<arr[mid]) high=mid-1;
+        }
+        if(flag==0) System.out.println(-1);
+        
+
+    }
+}
