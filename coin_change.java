5fa908543cb55bcc0855bbc0b7f1036761991698
Telukunta Vijay Abhinav <62216101+Abhinav-Telukunta@users.noreply.github.com>
2020-09-17 12:04:23 +0530

    DP-1

--- coin_change.java
+++ coin_change.java
@@ -1,3 +1,12 @@
+/*
+Given coins of different denomination and a target value, write a program to get the minimum number of coins to reach target.
+If target is not reached print -1
+ex: 1 2 5
+    11
+1,2 and 5 are different coins available and we have to reach 11 from 1,2 or 5 rupee coins in minimum possible way
+ans=5+5+1=3 coins required so ans is 3
+
+*/
 import java.util.*;
 class coin_change{
     public static void main(String args[]){
